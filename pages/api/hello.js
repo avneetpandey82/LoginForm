e80ac24a5e0ecc82816dbde9df084405827615ee
avneetpandey82 <avneetpandey82@gmail.com>
2024-10-16 00:20:30 -0400

    test_change

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,6 +4,7 @@ import clientPromise from "../../lib/mongodb";
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
+      //ss
       const client = await clientPromise;
       const db = client.db("test");
       const newItem = JSON.parse(req.body);
